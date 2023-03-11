6abb3869387f2ddc2f08a39096a0157a6878e0ff
Seul-Seul <90150969+Seul-Seul@users.noreply.github.com>
2023-03-12 04:41:13 +0900

    백준 1546

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,20 @@ import unit1.CaseConversion;
 
 import java.util.*;
 public class Main {
-
     public static void main(String[] args) {
-        CaseConversion T = new CaseConversion();
+        Main T = new Main();
         Scanner kb = new Scanner(System.in);
+
         System.out.print("입력 : ");
-        String str = kb.next();
-        System.out.println(T.solution2(str));
+        int N = kb.nextInt();
+        long sum = 0;
+        long max = 0;
+
+        for(int i = 0 ; i < N ; i++){
+            int temp = kb.nextInt();
+            if(temp > max)  max = temp;
+            sum += temp;
+        }
+        System.out.println("결과: " + sum / max * 100.0 / N);
     }
 }
